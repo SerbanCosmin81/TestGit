409c76e93fc2f79d0a35bcfdba91386b4d088f8f
Cosmin Serban <serbancosmin2008@gmail.com>
2019-05-11 17:48:23 +0300

    initial commit

---
+++ MyFirstGit/MyFirstGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This is my first GIT");
+        }
+    }
+}
